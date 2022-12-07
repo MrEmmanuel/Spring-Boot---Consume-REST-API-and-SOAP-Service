45d748c4337202e786e138fb3fc704a0e732669e
MrEmmanuel <sejesoo@gmail.com>
2022-12-08 00:37:46 +0200

    Made changes to TEST API part

--- src/main/java/springboot/consumeAPI/controller/Controller.java
+++ src/main/java/springboot/consumeAPI/controller/Controller.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springboot.consumeAPI.service.UserService;
@@ -15,8 +16,8 @@ public class Controller {
     UserService userService;
 
     @GetMapping("/repos")
-    public ResponseEntity<String> getRepos(String user)
-    {
+    public ResponseEntity<String> getRepos(@PathVariable String user) {
+
         return new ResponseEntity<String>(userService.getRepos(user), HttpStatus.OK);
     }
 
--- src/main/java/springboot/consumeAPI/service/UserServiceImpl.java
+++ src/main/java/springboot/consumeAPI/service/UserServiceImpl.java
@@ -1,9 +1,17 @@
 package springboot.consumeAPI.service;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Locale;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -11,26 +19,34 @@ public class UserServiceImpl implements UserService {
     @Override
     public String getRepos(String user) {
         String url = String.format("https://api.github.com/users/%s/repos", "MrEmmanuel");
-        String repos = consume(url).toString();
+        String repos = consume(url, "name");
         return repos;
     }
 
     @Override
     public String getCommits(String user, String repoName) {
         String url = String.format("https://api.github.com/repos/%s/%s/commits", "MrEmmanuel", "git-basic-exercises");
-        String commits =  consume( url).toString();
+        String commits =  consume( url, "message");
         return commits;
     }
 
-    public String consume(String url){
+    public String consume(String url, String string){
         RestTemplate restTemplate = new RestTemplate();
-        String response
-                = restTemplate.getForObject(url, String.class);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept","application/vnd.github+json");
+        HttpEntity request = new HttpEntity(headers);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, request, String.class);
+        JSONArray jsonArray = new JSONArray(response.getBody());
+        String key = string.toLowerCase();
 
-        System.out.println(response);
-
-        return response;
+        for (int i=0; i<jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            if(key.equals("name")) {
+                System.out.println(jsonObject.getString("name"));
+            }else{
+                System.out.println(jsonObject.getJSONObject("commit").getString("message"));
+            }
+        }
+        return response.toString();
     }
-
-
 }
